69fbe50ece233a71a8cb529d1ac58f3674ff5f3d
najmann <ales.najmann@homecredit.net>
2014-11-01 18:55:35 +0100

    lat,lon support

--- proxy/bin/lepsimistoproxy.dart
+++ proxy/bin/lepsimistoproxy.dart
@@ -5,7 +5,7 @@ import 'package:route/url_pattern.dart';
 
 const port = 8081;
 
-final tipsPattern = new UrlPattern(r'/tips\/?');
+final tipsPattern = new UrlPattern(r'/tips/@(\d+)/(\d+)\/?');
 final tipPattern = new UrlPattern(r'/tips/(\d+)\/?');
 
 void main() {
@@ -19,20 +19,24 @@ void main() {
 }
 
 tips(HttpRequest req) {
+  var path = tipsPattern.parse(req.uri.path);
+  var latitude = path[0];
+  var longitude = path[1];
   var client = new HttpClient();
-  client.getUrl(Uri.parse("http://beta-api.lepsimisto.cz/v1/announcement?page=1&page_size=5&lat=50&lon=14&announcement_kind=3"))
+  client.getUrl(Uri.parse("http://beta-api.lepsimisto.cz/v1/announcement?page=1&page_size=5&lat=${latitude}&lon=${longitude}&announcement_kind=3"))
   .then((clientReq) => clientReq.close())
   .then((HttpClientResponse response) {
     req.response.headers.add("Access-Control-Allow-Origin", "*"); // support CORS
     req.response.headers.contentType = new ContentType("application", "json", charset: "utf-8");
     response.transform(UTF8.decoder).listen((contents) {
-      req.response..write(contents.toString())..close();
-    }, onDone: () => req.response.close());
+      req.response..write(contents)..close();
+    });
   });
 }
 
 tipDetail(HttpRequest req) {
-  var tipId = tipPattern.parse(req.uri.path)[0];
+  var path = tipPattern.parse(req.uri.path);
+  var tipId = path[0];
   var client = new HttpClient();
   client.getUrl(Uri.parse("http://beta-api.lepsimisto.cz/v1/announcement/${tipId}"))
   .then((clientReq) => clientReq.close())
@@ -40,8 +44,8 @@ tipDetail(HttpRequest req) {
     req.response.headers.add("Access-Control-Allow-Origin", "*"); // support CORS
     req.response.headers.contentType = new ContentType("application", "json", charset: "utf-8");
     response.transform(UTF8.decoder).listen((contents) {  
-      req.response..write(contents.toString());
-    }, onDone: () => req.response.close());
+      req.response..write(contents)..close();
+    });
   });
 }
 
