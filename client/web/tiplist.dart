6b4e0c4cb1ffdc245a68c5d7d88d0841b4b85c67
Martin Malý <martin.maly@pnb.cz>
2014-11-01 17:23:45 +0100

    Web - general refactoring.

--- client/web/tiplist.dart
+++ client/web/tiplist.dart
@@ -1,12 +1,8 @@
 import 'package:polymer/polymer.dart';
-//import 'package:paper_elements/paper_input.dart';
-//import 'package:core_elements/core_input.dart';
-import 'package:core_elements/core_toolbar.dart';
-import 'package:core_elements/core_header_panel.dart';
-import 'package:core_elements/core_toolbar.dart';
-import 'package:paper_elements/paper_icon_button.dart';
 import 'dart:html';
 import 'dart:convert';
+import 'dart:core';
+import 'package:intl/intl.dart';
 
 /**
  * A Polymer element to print a list of Tips.
@@ -29,14 +25,6 @@ class TipList extends PolymerElement {
     var request = HttpRequest.getString(url).then(onTipListLoaded);
   }
   
-  void loadTipDetail(int id) {
-     //var url = "http://beta-api.lepsimisto.cz/v1/announcement?page=1&lat=" + latitude.toString() + "&lon=" + longitude.toString() + "&announcement_kind=3";
-     var url = "json/tip-" + id.toString() + ".json";
-     
-     // call the web server asynchronously
-     var request = HttpRequest.getString(url).then(onDetailLoaded);
-   }
-  
   void onTipListLoaded(String responseText) {
     Map parsedJson = JSON.decode(responseText);
     List announcements = parsedJson["announcements"];
@@ -44,22 +32,28 @@ class TipList extends PolymerElement {
       loadTipDetail(announcement['id']);
     }
   }
+  
+  void loadTipDetail(int id) {
+    //var url = "http://beta-api.lepsimisto.cz/v1/announcement?page=1&lat=" + latitude.toString() + "&lon=" + longitude.toString() + "&announcement_kind=3";
+    var url = "json/tip-" + id.toString() + ".json";
+    
+    // call the web server asynchronously
+    var request = HttpRequest.getString(url).then(onDetailLoaded);
+  }
  
   void onDetailLoaded(String responseText) {
     Map parsedJson = JSON.decode(responseText);
     Map announcement = parsedJson["announcement"];
     
     // Set default photo
-    if (announcement['photos'].length > 0) {
-      announcement['photo'] = announcement['photos'][0]['urls'][0] + "/w100h100";
-    } else {
-      announcement['photo'] = 'http://beta-www.lepsimisto.cz/Content/images/avatar_small.png';
-    }
+    setTipPhotoUrl(announcement);
     
-    // correct url - we want to run against beta
-    announcement['url'] = announcement['url'].toString().replaceFirst("http://www-lepsimisto-cz.azurewebsites.net", "http://beta-www.lepsimisto.cz");
+    // We run against beta
+    announcement['url'] = fixBrokenTipUrl(announcement['url']);
     
-    
+    // Format datetime to human readable format
+    announcement['created_formatted'] = formatDate(announcement['created']);
+ 
     tips.add(announcement);
 //    if (tips.length == 0) {
 //      tips.add(announcement);
@@ -71,5 +65,25 @@ class TipList extends PolymerElement {
 //        }
 //      }
 //    }
+  }
+  
+  // Sets tip photo or default, is tip does not have any
+  void setTipPhotoUrl(Map announcement) {
+    if (announcement['photos'].length > 0) {
+         announcement['photo'] = announcement['photos'][0]['urls'][0] + "/w100h100";
+       } else {
+         announcement['photo'] = 'http://beta-www.lepsimisto.cz/Content/images/avatar_small.png';
+       }
+  }
+  
+  // Formats unix timestamp to human readable form 
+  String formatDate(int timestamp) {
+    var datetime = new DateTime.fromMillisecondsSinceEpoch(timestamp);
+    return new DateFormat('dd.MM.yyyy').format(datetime);
+  }
+  
+  // Fixes broken tip url, we run against Beta
+  String fixBrokenTipUrl(String url) {
+    return url.replaceFirst("http://www-lepsimisto-cz.azurewebsites.net", "http://beta-www.lepsimisto.cz");
   }
 }
