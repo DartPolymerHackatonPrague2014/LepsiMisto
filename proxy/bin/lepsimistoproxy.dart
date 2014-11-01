f76c0eb4ef88d937de8e27422a4b8d71af40a138
Martin Malý <martin.maly@pnb.cz>
2014-11-01 18:20:16 +0100

    Proxy - read only 5 first tips.

--- proxy/bin/lepsimistoproxy.dart
+++ proxy/bin/lepsimistoproxy.dart
@@ -20,7 +20,7 @@ void main() {
 
 tips(HttpRequest req) {
   var client = new HttpClient();
-  client.getUrl(Uri.parse("http://beta-api.lepsimisto.cz/v1/announcement?page=1&page_size=500&lat=50&lon=14&announcement_kind=3"))
+  client.getUrl(Uri.parse("http://beta-api.lepsimisto.cz/v1/announcement?page=1&page_size=5&lat=50&lon=14&announcement_kind=3"))
   .then((clientReq) => clientReq.close())
   .then((HttpClientResponse response) {
     req.response.headers.add("Access-Control-Allow-Origin", "*"); // support CORS
