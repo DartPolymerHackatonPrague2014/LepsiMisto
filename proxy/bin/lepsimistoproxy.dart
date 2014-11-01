9b46f73e9fdfbc57f32b3c5ee5bb9f7ab7600463
Martin Malý <martin.maly@pnb.cz>
2014-11-01 19:17:50 +0100

    Proxy - update URL to production API.

--- proxy/bin/lepsimistoproxy.dart
+++ proxy/bin/lepsimistoproxy.dart
@@ -24,7 +24,7 @@ tips(HttpRequest req) {
   var latitude = path[0];
   var longitude = path[1];
   //var client = new HttpClient();
-  client.getUrl(Uri.parse("http://beta-api.lepsimisto.cz/v1/announcement?page=1&page_size=5&lat=${latitude}&lon=${longitude}&announcement_kind=3"))
+  client.getUrl(Uri.parse("http://api.lepsimisto.cz/v1/announcement?page=1&page_size=50&lat=${latitude}&lon=${longitude}&announcement_kind=3"))
   .then((clientReq) => clientReq.close())
   .then((HttpClientResponse response) {
     req.response.headers.add("Access-Control-Allow-Origin", "*"); // support CORS
@@ -39,7 +39,7 @@ tipDetail(HttpRequest req) {
   var path = tipPattern.parse(req.uri.path);
   var tipId = path[0];
   // var client = new HttpClient();
-  client.getUrl(Uri.parse("http://beta-api.lepsimisto.cz/v1/announcement/${tipId}"))
+  client.getUrl(Uri.parse("http://api.lepsimisto.cz/v1/announcement/${tipId}"))
   .then((clientReq) => clientReq.close())
   .then((HttpClientResponse response) {
     req.response.headers.add("Access-Control-Allow-Origin", "*"); // support CORS
